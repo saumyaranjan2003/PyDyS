86d69d7a35c064a8de9bc70655be112fe6864587
Saumya Ranjan Mohapatra <111070899+saumyaranjan2003@users.noreply.github.com>
2024-01-22 13:25:23 +0530

    test
    testing

---
+++ float.py
@@ -0,0 +1,19 @@
+num1 = 5.0
+num2 = 2.5
+result = num1+num2
+print("Add",result);
+
+result = num1-num2
+print("Sub",result);
+
+result = num1/num2
+print("Div",result);
+
+result = num1%num2
+print("Mod",result);
+
+result = num1*num2
+print("mul",result);
+
+result = round(2.3678678123)
+print("Rouded",result)
---
+++ test.py
@@ -0,0 +1,3 @@
+arn = "arn:aws:iam::12345678901:user/johndoe"
+print(arn.split("/")[0])
+print(arn.split("/")[1])
